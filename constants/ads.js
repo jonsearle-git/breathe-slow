5320531f521a3bee5cef0c6ae346c10e51cb3ced
jon <jon_searle@outlook.com>
2026-04-06 17:33:53 +0100

    removed comment

--- constants/ads.js
+++ constants/ads.js
@@ -1,7 +1,7 @@
 import { Platform } from 'react-native';
 
 const REWARDED_AD_UNIT_ID = Platform.select({
-  ios: 'ca-app-pub-9015796764047989/6516519105', // TEST ID — replace before release
+  ios: 'ca-app-pub-9015796764047989/6516519105', 
   android: 'ca-app-pub-3940256099942544/5224354917', // TEST ID — replace before release
 });
 
