3564fcfaad0755ce48ac75f0fb2c2bdab00f55ba
jon <jon_searle@outlook.com>
2026-04-06 19:34:03 +0100

    Adding Android AppId AND ad id

--- constants/ads.js
+++ constants/ads.js
@@ -2,7 +2,7 @@ import { Platform } from 'react-native';
 
 const REWARDED_AD_UNIT_ID = Platform.select({
   ios: 'ca-app-pub-9015796764047989/6516519105', 
-  android: 'ca-app-pub-3940256099942544/5224354917', // TEST ID — replace before release
+  android: 'ca-app-pub-9015796764047989/5272554348',
 });
 
 export { REWARDED_AD_UNIT_ID };
